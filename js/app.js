c4b517d9604ab358bbd25694b3baba133c17acc1
Felipe Santos <felipe.santospinheiro@gmail.com>
2025-02-02 14:21:55 -0300

    Subindo projeto

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let totalGeral = 0;
+limpar();
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = parseFloat(produto.split('R$')[1]); // Converte para número
+    let quantidade = parseInt(document.getElementById('quantidade').value); // Converte para número
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco.toLocaleString('pt-BR', { minimumFractionDigits:2, maximumFractionDigits:2})}</span>
+        </section>`;
+    //atualizar o valor total
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+    document.getElementById('quantidade').value = '';
+
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+
+}
